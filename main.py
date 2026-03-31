071c50ab0d21dad41267090e6ecfbd9916d5c028
prakhar-paliwal-07 <prakharpaliwal858@gmail.com>
2026-03-31 14:49:43 +0530

    Refine user input prompts in main.py
    Updated user prompts for clarity and consistency.

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def run():
         print_menu()
         choice = input("Enter your choice: ")
         if choice =="1":
-            desc =input("Description: ")
+            desc =input("Description about : ")
             amt=float(input("Amount: "))
-            cat=input("Category: " )
+            cat=input("Category for: " )
             date=input("Date (YYYY-MM-DD): ")
             add_expense   (expenses, desc, amt, cat, date)
         elif choice=="2":
-            src = input("Source: ")
-            amt =float(input("Amount: "))
+            src = input("Source of: ")
+            amt =float(input("Amount in : "))
             date = input("Date (YYYY-MM-DD): ")
             add_income(incomes, src,  amt,  date) 
         elif choice== "3":
-            print("Total Expenses:", total_expenses(expenses))
-            print("Total Income:", total_incomes(incomes))
-            print("Net Balance:", net_balance(expenses, incomes))
+            print("Total Expenses of u :", total_expenses(expenses))
+            print("Total Income of u :", total_incomes(incomes))
+            print("Net Balance of u :", net_balance(expenses, incomes))
         elif choice ==   "0"  :
             save_data(expenses, incomes)
             print("Data saved.. Program ending....")
@@ -34,7 +34,7 @@ def run():
 
 
         else:
-            print("Invalid choice. Try again.")
+            print("Invalid choice, we are sorry . Try again.")
 
 if __name__ == "__main__":
     run()
